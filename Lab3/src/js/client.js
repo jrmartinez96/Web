e2bdb41db3080b8584b4b1a7bbf2ca41e70c48d0
jrmartinez96 <pepemart1996@gmail.com>
2018-07-18 18:15:22 -0600

    Primer lab 3 commit

---
+++ Lab3/src/js/client.js
@@ -0,0 +1,12 @@
+/*
+==========================================
+    Universidad del Valle de Guatemala
+    Sistemas y tecnologias web
+
+    Jose Martinez
+    15163
+
+    Laboratorio #2
+    client.js
+==========================================
+*/
