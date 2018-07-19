28d796537c80e49b9d00a3d9e4ac9388abf49f26
jrmartinez96 <pepemart1996@gmail.com>
2018-07-18 19:04:20 -0600

    Segundo commit lab3

--- Lab3/src/js/client.js
+++ Lab3/src/js/client.js
@@ -10,3 +10,36 @@
     client.js
 ==========================================
 */
+
+const state = {
+
+};
+
+const render = lState => {
+  // Clear previous root content
+  if (root.hasChildNodes()) {
+    root.innerHTML = null;
+  }
+
+  const navBar = document.createElement('div');
+  navBar.className = 'nav-bar';
+
+  const allTab = document.createElement('div');
+  allTab.className = 'nav-item left';
+  allTab.innerHTML = "All";
+
+  const completedTab = document.createElement('div');
+  completedTab.className = 'nav-item middle';
+  completedTab.innerHTML = "Completed";
+
+  const activeTab = document.createElement('div');
+  activeTab.className = 'nav-item right';
+  activeTab.innerHTML = "Active";
+
+  root.appendChild(navBar);
+  navBar.appendChild(allTab);
+  navBar.appendChild(completedTab);
+  navBar.appendChild(activeTab);
+};
+
+render(state);
