fdc2a1dc950fe1aeed4c056e18db091a2836e4b7
jrmartinez96 <pepemart1996@gmail.com>
2018-10-12 07:39:40 -0600

    Agregar modelo de Chisme

--- lab9/backend/chismes_project/chismes_app/models.py
+++ lab9/backend/chismes_project/chismes_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Chisme(models.Model):
+    title = models.CharField(max_length=75)
+    description = models.CharField(max_length=500)
+    creation_datetime = models.DateTimeField(auto_now=True)
